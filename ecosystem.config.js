3f8486944518bae4d2edfb7609e8fc96d2af0d32
Geeks_U <cncdre11@outlook.com>
2025-05-10 12:09:42 +0800

    cicd测试

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,19 @@
 module.exports = {
   apps: [
     {
-      name: 'personal-site-backend',  // 应用名称
-      script: 'dist/index.js',          // 编译后的 JavaScript 文件路径
-      instances: 'max',               // 启动最大实例数（'max' 表示根据 CPU 核心数自动设置）
-      exec_mode: 'cluster',           // 使用 cluster 模式，支持负载均衡
-      watch: true,                    // 监听文件变化并重启应用
+      name: 'personal_site_backend', // 应用名称
+      script: 'dist/index.js',       // 编译后的 JavaScript 文件路径
+      instances: 'max',              // 启动最大实例数（'max' 表示根据 CPU 核心数自动设置）
+      exec_mode: 'cluster',          // 使用 cluster 模式，支持负载均衡
+      watch: ['dist'],               // 仅监控 dist 文件夹中的变化
+      ignore_watch: ['node_modules', 'logs'], // 忽略 node_modules 和 logs 文件夹的变化
       env: {
-        NODE_ENV: 'development',      // 开发环境变量
+        NODE_ENV: 'development',
         HOST: '0.0.0.0',
         PORT: 9000
       },
       env_production: {
-        NODE_ENV: 'production',       // 生产环境变量
+        NODE_ENV: 'production',
         HOST: '0.0.0.0',
         PORT: 8000
       },
