49ffca9a0f2b1629d480fb028de92507a45f4df8
Geeks_U <cncdre11@outlook.com>
2025-05-10 10:14:14 +0800

    ip，port环境变量配置

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import app from "./app"
-const port = 8000
 
-// 启动服务器
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`)
+const host = process.env.HOST || '0.0.0.0'
+const port = parseInt(process.env.PORT || '8000', 10)
+
+app.listen(port, host, () => {
+  console.log(`Server is running at http://${host}:${port}`)
 })
