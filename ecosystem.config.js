2c1e53be5a659035371fa11d3ae07a1d2557910c
Geeks_U <cncdre11@outlook.com>
2025-05-10 09:48:18 +0800

    pm2配置文件编写

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [
+    {
+      name: 'personal-siteibackend',  // 应用名称
+      script: 'dist/app.js',          // 编译后的 JavaScript 文件路径
+      instances: 'max',               // 启动最大实例数（'max' 表示根据 CPU 核心数自动设置）
+      exec_mode: 'cluster',           // 使用 cluster 模式，支持负载均衡
+      watch: true,                    // 监听文件变化并重启应用
+      env: {
+        NODE_ENV: 'development',      // 开发环境变量
+      },
+      env_production: {
+        NODE_ENV: 'production',       // 生产环境变量
+      },
+    },
+  ],
+};
