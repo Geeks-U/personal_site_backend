d1a489ba290eda8bc2f2a76a228b59dc548e3b36
Geeks_U <cncdre11@outlook.com>
2025-05-10 13:13:30 +0800

    cicd测试

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,8 @@ module.exports = {
     {
       name: 'personal_site_backend', // 应用名称
       script: 'dist/index.js',       // 编译后的 JavaScript 文件路径
-      instances: 'max',              // 启动最大实例数（'max' 表示根据 CPU 核心数自动设置）
+      // 两实例内存不共享，会导致在一个实例内存中保存的email验证码在另一个实例上访问不到，所以只使用一个实例
+      instances: '1',              // 启动最大实例数（'max' 表示根据 CPU 核心数自动设置）
       exec_mode: 'cluster',          // 使用 cluster 模式，支持负载均衡
       watch: ['dist'],               // 仅监控 dist 文件夹中的变化
       ignore_watch: ['node_modules', 'logs'], // 忽略 node_modules 和 logs 文件夹的变化
