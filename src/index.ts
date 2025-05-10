9b5675f2df0ff597836c82c36357a04582c7bd9b
Geeks_U <cncdre11@outlook.com>
2025-05-10 12:23:17 +0800

    cicd测试

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,6 @@ const host = process.env.HOST || '0.0.0.0'
 const port = parseInt(process.env.PORT || '8000', 10)
 
 app.listen(port, host, () => {
-  console.log(`Server is running at http://${host}:${port}`)
+  const now = new Date().toISOString()
+  console.log(`[${now}] Server is running at http://${host}:${port}`)
 })
